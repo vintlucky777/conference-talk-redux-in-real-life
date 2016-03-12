0021b19a1c133f19107ca2f357adf7c1f2f8711d
Victor Klochikhin <vintlucky777@gmail.com>
2016-03-12 18:46:27 +0200

    basic styles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import configureStore from './store/configureStore';
-import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-
-const store = configureStore();
+import store from './store';
+import './styles/styles.less'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 
 render(
   <Provider store={store}>
